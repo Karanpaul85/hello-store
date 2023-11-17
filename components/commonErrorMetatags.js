edc90d698cb6083d35040e76456e25520bdacaea
Karan_Paul <karan.paul@fnp.com>
2023-11-17 20:58:48 +0530

    sddssdsd

--- components/commonErrorMetatags.js
+++ components/commonErrorMetatags.js
@@ -11,13 +11,13 @@ export const ogErrorMetaTags = (errorData) => {
       <meta name="twitter:title" content={errorData} />
       <meta property="og:description" content={errorData} />
       <meta name="twitter:description" content={errorData} />
-      <link rel="icon" type="image/png" href="/assets/images/logo.jpg" />
-        <link rel="apple-touch-icon" href="/assets/images/logo.jpg" />
-        <meta property="og:image" content="/assets/images/logo.jpg" />
+      <link rel="icon" type="image/png" href="https://breakingnewsapp.netlify.app/assets/images/logo.jpg" />
+        <link rel="apple-touch-icon" href="https://breakingnewsapp.netlify.app/assets/images/logo.jpg" />
+        <meta property="og:image" content="https://breakingnewsapp.netlify.app/assets/images/logo.jpg" />
         <meta
           name="twitter:image"
           property="og:image"
-          content="/assets/images/logo.jpg"
+          content="https://breakingnewsapp.netlify.app/assets/images/logo.jpg"
         />
     </>
   );
