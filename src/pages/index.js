cd47be8f4e75716d3c65219d8ad8eff2faea54be
Karan_Paul <karan.paul@fnp.com>
2023-11-17 12:50:19 +0530

    adding layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,21 @@
 // pages/index.js
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setNewsData, fetchData } from "../redux/slices/newsSlice";
+import Layout from "../../components/Layout";
+import { fetchData } from "../redux/slices/newsSlice";
 import { wrapper } from "../utils/withRedux";
-import api from "../utils/api";
 
 const HomePage = ({ data }) => {
-  // const dispatch = useDispatch();
-  // const myData = useSelector((state) => state.newSlice.newsData);
-  // const loading = useSelector((state) => state.newSlice.loading);
-  // const error = useSelector((state) => state.newSlice.error);
-  // useEffect(() => {
-  //   // Dispatch the fetchData async thunk
-  //   dispatch(fetchData());
-  // }, [dispatch]);
-
   return (
-    <div>
+    <Layout>
       {data &&
         data.map((item) => {
-          return <p key={item.title}>{item.title} + 1</p>;
+          return <p key={item.title}>{item.title}</p>;
         })}
-    </div>
+    </Layout>
   );
 };
 export const getServerSideProps = wrapper.getServerSideProps(
   (store) => async () => {
     try {
-      // Manually dispatch the fetchData async thunk on the server side
       const serverData = await store.dispatch(fetchData());
       return {
         props: { data: serverData.payload },
