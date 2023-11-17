ebd7a7c93b5dccf0875eef5d16cf7884f96112b7
Karan_Paul <karan.paul@fnp.com>
2023-11-17 20:42:25 +0530

    logo on error

--- components/commonErrorMetatags.js
+++ components/commonErrorMetatags.js
@@ -11,6 +11,14 @@ export const ogErrorMetaTags = (errorData) => {
       <meta name="twitter:title" content={errorData} />
       <meta property="og:description" content={errorData} />
       <meta name="twitter:description" content={errorData} />
+      <link rel="icon" type="image/png" href="/assets/images/logo.webp" />
+        <link rel="apple-touch-icon" href="/assets/images/logo.webp" />
+        <meta property="og:image" content="/assets/images/logo.webp" />
+        <meta
+          name="twitter:image"
+          property="og:image"
+          content="/assets/images/logo.webp"
+        />
     </>
   );
 };
