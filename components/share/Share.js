e58395619d6cfd73129d5bf8626fecec8065a0ec
Karan_Paul <karan.paul@fnp.com>
2023-11-21 14:41:05 +0530

    add material UI and create single news component and share btn

---
+++ components/share/Share.js
@@ -0,0 +1,25 @@
+import { Share } from "@mui/icons-material";
+import styles from "./share.module.css";
+import { useEffect, useState } from "react";
+const ShareBTN = () => {
+  const [isRealDevice, setIsRealDevice] = useState(false);
+  useEffect(() => {
+    navigator.share && setIsRealDevice(true);
+  }, []);
+
+  const desktopShare = () => {
+    console.log("desktop");
+  };
+  const realDevice = () => {
+    alert("realDevice");
+  };
+  return (
+    <button
+      className={styles.shareBtn}
+      onClick={isRealDevice ? realDevice : desktopShare}
+    >
+      <Share />
+    </button>
+  );
+};
+export default ShareBTN;
