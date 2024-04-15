03138e77592dc0093feb76808dcc235f8db5a812
Karan_Paul <karan.paul@fnp.com>
2024-04-15 13:20:03 +0530

    create profile page

---
+++ src/pages/profile/index.jsx
@@ -0,0 +1,11 @@
+"use client";
+import Layout from "../../../components/Layout";
+
+const Profile = () => {
+  return (
+    <Layout topright={false} showBottomBar={false}>
+      Profile Page
+    </Layout>
+  );
+};
+export default Profile;
