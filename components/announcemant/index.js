05e1ad52f2222ed2008b7d87731b302870d4bab5
Karan_Paul <karan.paul@fnp.com>
2024-10-21 16:25:29 +0530

    Add Announcement component

---
+++ components/announcemant/index.js
@@ -0,0 +1,12 @@
+import styles from "./announcement.module.css";
+const Announcement = () => {
+  return (
+    <div className={styles.announcement}>
+      For advertising inquiries or banner display opportunities on this website,
+      please contact:- Contact Person: <span>Karan Paul</span>, Email:
+      <a href="mailto:karanpaul85@gmail.com">karanpaul85@gmail.com</a>
+      Phone: <a href="tel:+918130464110">+91 8130464110</a>
+    </div>
+  );
+};
+export default Announcement;
